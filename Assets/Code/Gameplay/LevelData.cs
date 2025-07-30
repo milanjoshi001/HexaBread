bc9b321fbc78b36ef88787d7f97cbeb00fdb11a8
Milan <milan@tentworks.io>
2025-07-30 07:35:11 +0530

    Updated level assets

--- Assets/Code/Gameplay/LevelData.cs
+++ Assets/Code/Gameplay/LevelData.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class LevelData : ScriptableObject
 {
     [SerializeField] private int _levelCompleteRequirement;
-    
+    [SerializeField] private GameObject _levelGrid;
+
     public int LevelCompleteRequirement => _levelCompleteRequirement;
+    public GameObject LevelGrid => _levelGrid;
 }
