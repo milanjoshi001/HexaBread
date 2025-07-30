75f3ab379730923a04eb878a413d8c85140f3cbd
Milan <milan@tentworks.io>
2025-07-30 07:35:53 +0530

    Updated level manager

--- Assets/Code/Gameplay/LevelManager.cs
+++ Assets/Code/Gameplay/LevelManager.cs
@@ -7,12 +7,32 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField] private LevelDataLibrary _levelDataLibrary;
 
+
+    private int _currentLevelIndex = 0;
+
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
     }
 
-    public int GetLevelCompleteRequirement() => _levelDataLibrary.LevelDataList[0].LevelCompleteRequirement;
+    private void Start()
+    {
+        LevelComplete.OnLevelComplete += NextLevelCounter;
+        GridManager.Instance.LoadGrid(_levelDataLibrary.LevelDataList[0].LevelGrid);
+    }
+
+    private void NextLevelCounter()
+    {
+        _currentLevelIndex++;
+        
+        GameplayUI.Instance.ResetLevelText();
+    }
+
+
+    public LevelData GetNextLevel() => _levelDataLibrary.LevelDataList[_currentLevelIndex];
+
+    public LevelData GetSameLevel() =>
+        _levelDataLibrary.LevelDataList[_currentLevelIndex == 0 ? _currentLevelIndex : _currentLevelIndex - 1];
 
 }
