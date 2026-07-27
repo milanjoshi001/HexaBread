8cf994552d159ff4848585f6f3fc12424c0be442
Milan <mjgames213@gmail.com>
2026-07-27 09:39:43 +0530

    Updated main menu UI

--- Assets/Code/UI/MainMenuUI.cs
+++ Assets/Code/UI/MainMenuUI.cs
@@ -6,13 +6,30 @@ using UnityEngine.UI;
 
 public class MainMenuUI : Singleton<MainMenuUI>
 {
+    [SerializeField] private Image _backgroundImage;
+    
     [Header("Top Elements")] 
     [SerializeField] private TextMeshProUGUI _coinsText;
     
+    [Header("Bottom Elements")] 
+    [SerializeField] private Button _homeButton;
+    [SerializeField] private GameObject _homeLableText;
+    [SerializeField] private GameObject _homePanel;
+    [SerializeField] private Button _shopButton;
+    [SerializeField] private GameObject _shopLableText;
+    [SerializeField] private GameObject _shopPanel;
+    [SerializeField] private Button _levelButton;
+    [SerializeField] private GameObject _levelLableText;
+    [SerializeField] private GameObject _levelPanel;
+    
+    
     [Header("Elements")] 
-    [SerializeField] private Image _backgroundImage;
     [SerializeField] private TextMeshProUGUI _levelText;
     [SerializeField] private Button _playButton;
+    
+    private Button _currentButton;
+    private GameObject _currentPanel;
+    private GameObject _currentText;
 
     private void Start()
     {
@@ -20,6 +37,11 @@ public class MainMenuUI : Singleton<MainMenuUI>
 
         _backgroundImage.SetVerticesDirty();
         LoadCoins();
+        
+        _homeButton.onClick.AddListener(HomeButtonCallback);
+        _shopButton.onClick.AddListener(ShopButtonCallback);
+        _levelButton.onClick.AddListener(LevelButtonCallback);
+        HomeButtonCallback();
     }
 
     private void OnEnable()
@@ -30,7 +52,29 @@ public class MainMenuUI : Singleton<MainMenuUI>
     private void OnDestroy()
     {
         _playButton.onClick.RemoveListener(PlayButtonCallback);
+        _homeButton.onClick.RemoveListener(HomeButtonCallback);
+        _shopButton.onClick.RemoveListener(ShopButtonCallback);
+        _levelButton.onClick.RemoveListener(LevelButtonCallback);
     }
+
+    #region Callbacks
+
+    private void HomeButtonCallback()
+    {
+        Display(ref _homeButton, ref _homePanel, ref _homeLableText);
+    }
+
+    private void ShopButtonCallback()
+    {
+        Display(ref _shopButton, ref _shopPanel, ref _shopLableText);
+    }
+
+    private void LevelButtonCallback()
+    {
+        Display(ref _levelButton, ref _levelPanel, ref _levelLableText);
+    }
+
+    #endregion
     
     public void LoadCoins() => _coinsText.SetText($"{CoinsManager.Instance.Coins.TotalCoins}");
 
@@ -42,4 +86,38 @@ public class MainMenuUI : Singleton<MainMenuUI>
     }
 
     public void Activate(bool value) =>  gameObject.SetActive(value);
+
+    private void Display(ref Button button, ref GameObject panel, ref GameObject textObject)
+    {
+        if (_currentButton != null && _currentPanel != null)
+        {
+            _currentButton.interactable = true;
+            _currentPanel.SetActive(false);
+            _currentText.SetActive(false);
+            AnimateText(ref _currentButton, true);
+        }
+        
+        _currentButton = button;
+        _currentPanel = panel;
+        _currentText = textObject;
+        
+        _currentPanel.SetActive(true);
+        _currentText.SetActive(true);
+        _currentButton.interactable = false;
+        AnimateText(ref _currentButton, false);
+    }
+
+    private void AnimateText(ref Button button, bool originalPos)
+    {
+        var iconObject = button.transform.GetChild(0).gameObject;
+        var posObject = button.transform.GetChild(1);
+        var backObject = button.transform.GetChild(2);
+        
+        if(originalPos)
+            LeanTween.move(iconObject, backObject, 0.25f);
+        else
+            LeanTween.move(iconObject, posObject, 0.25f);
+        
+    }
+    
 }
