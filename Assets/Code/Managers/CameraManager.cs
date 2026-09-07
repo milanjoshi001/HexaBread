6d3f8918067cff1289617fc55bace9c31768cd23
Milan <mjgames213@gmail.com>
2026-09-07 23:36:23 +0530

    Implemented basic camera panning

--- Assets/Code/Managers/CameraManager.cs
+++ Assets/Code/Managers/CameraManager.cs
@@ -1,38 +1,86 @@
 using System;
 using Code.Utils;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Camera))]
 public class CameraManager : Singleton<CameraManager>
 {
-    [Header("Cafe Shop Settings")] 
-    [SerializeField] private Vector3 _cafeShopPosition;
-    [SerializeField] private Vector3 _cafeShopRotation;
-    
-    private Camera _camera;
-    private Vector3 _cameraPosition;
-    private Quaternion _cameraRotation;
+    [SerializeField] private float _panSpeed;
 
-    private void Start()
+    private Vector2 _lastPointerPosition;
+    private bool _isDragging;
+
+    private void Update()
     {
-        _camera = GetComponent<Camera>();
-        _cameraPosition = _camera.transform.position;
-        _cameraRotation = _camera.transform.rotation;
+        HandleMouse();
+        HandleTouch();
     }
 
-    public void ToggleProjection()
+    private void HandleMouse()
     {
-        _camera.orthographic = !_camera.orthographic;
+        if (Mouse.current == null)
+            return;
 
-        if (!_camera.orthographic)
+        Vector2 currentPosition = Mouse.current.position.ReadValue();
+
+        // Mouse pressed
+        if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            _camera.transform.position = _cafeShopPosition;
-            _camera.transform.rotation = Quaternion.Euler(_cafeShopRotation);
+            _isDragging = true;
+            _lastPointerPosition = currentPosition;
         }
-        else
+
+        // Mouse held + dragged
+        if (Mouse.current.leftButton.isPressed && _isDragging)
         {
-            _camera.transform.position = _cameraPosition;
-            _camera.transform.rotation = _cameraRotation;
+            Vector2 delta = currentPosition - _lastPointerPosition;
+
+            PanCamera(delta);
+
+            _lastPointerPosition = currentPosition;
         }
+
+        // Mouse released
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            _isDragging = false;
+        }
+    }
+
+    private void HandleTouch()
+    {
+        if (Touchscreen.current == null)
+            return;
+
+        var touch = Touchscreen.current.primaryTouch;
+
+        if (touch.press.wasPressedThisFrame)
+        {
+            _isDragging = true;
+            _lastPointerPosition = touch.position.ReadValue();
+        }
+
+        if (touch.press.isPressed && _isDragging)
+        {
+            Vector2 currentPosition = touch.position.ReadValue();
+            Vector2 delta = currentPosition - _lastPointerPosition;
+
+            PanCamera(delta);
+
+            _lastPointerPosition = currentPosition;
+        }
+
+        if (touch.press.wasReleasedThisFrame)
+        {
+            _isDragging = false;
+        }
+    }
+
+    private void PanCamera(Vector2 delta)
+    {
+        Vector3 movement = new Vector3(-delta.x, 0f, -delta.y);
+
+        transform.Translate(movement * _panSpeed * Time.deltaTime, Space.World);
     }
 }
